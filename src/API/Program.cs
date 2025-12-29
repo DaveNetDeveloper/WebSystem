78b3177e8397b93444c717dd57830de75f1703bc
David Martinez <dgomezma@gmail.com>
2025-12-29 01:34:09 +0100

    refactor: Clean sensitive keys from configuration files. Implements local environment variables instead (dotnet user-secrets).

--- src/API/Program.cs
+++ src/API/Program.cs
@@ -166,9 +166,15 @@ builder.Services.AddCors(options =>
             .WithOrigins("https://localhost:7175")   
             .AllowAnyHeader()
             .AllowAnyMethod()
-            .AllowCredentials();// NECESARIO para cookies
+            .AllowCredentials();// NECESARIO para cookies seguras(server)
     });
 });
+
+builder.Services
+    .AddOptions<AppConfiguration>()
+    .Bind(builder.Configuration.GetSection("ServidorSmtp"))
+    .Validate(o => !string.IsNullOrEmpty(o.ServidorSmtp), "ServidorSmtp missing")
+    .ValidateOnStart();
 
 //var sp = builder.Services.BuildServiceProvider();
 //var authOptions = sp.GetRequiredService<Microsoft.Extensions.Options.IOptions<Microsoft.AspNetCore.Authentication.AuthenticationOptions>>().Value;
