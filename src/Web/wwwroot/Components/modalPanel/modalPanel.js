71dee4de10b4fba56fe4b85e9be04ebba2bf0c7f
David Martinez <dmartin3zgomez@gmail.com>
2026-01-13 17:27:12 +0100

    feat: Adds a new button in order to show info about scan QR Code v´ia modalPanel. Also modifies modalPanel component in order to allow an image as content.

--- src/Web/wwwroot/Components/modalPanel/modalPanel.js
+++ src/Web/wwwroot/Components/modalPanel/modalPanel.js
@@ -61,10 +61,16 @@
         });
     }
 
-    setContent(title, message, type) {
+    setContent(title, message, type, imagen) {
 
         this.shadowRoot.querySelector('h3').textContent = title;
         this.shadowRoot.querySelector('p').textContent = message;
+
+        // solamente si viene imagen la cargamos y mostramos
+        if (imagen != null && imagen != '') {
+            this.shadowRoot.querySelector('img').src = imagen;
+            this.shadowRoot.querySelector('.modalImagen').style.visibility = 'visible';
+        }  
 
         const closeBtn = this.shadowRoot.querySelector('.cerrar');
         const btnCancel = this.shadowRoot.querySelector('.cancel');
